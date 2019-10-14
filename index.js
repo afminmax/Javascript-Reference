bf22968a464404d47069df30f6059b1ca4101779
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2019-10-14 23:19:32 +0200

    first adventures in javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+        // Javascript comment here
+        let title = 'Waterpolo World';
+        let isApproved = true; //Boolean literal
+        //let age = 45;
+        let persons = {
+            firstname: 'Arya',
+            number: 23
+        };
+
+
+        console.log('Hello World from the Shrike!!!!');
